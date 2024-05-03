f292867a04b93d46582743bd2c8cb017fefa6097
pedraamchamsaz <151649370+pedraamchamsaz@users.noreply.github.com>
2024-05-03 17:16:35 +0100

    Server set up

--- routes/events.js
+++ routes/events.js
@@ -1,4 +1,21 @@
 const express = require('express')
 const router = express.Router()
 
+const events = [
+    { name: "Italian Pasta Making", country: "Italy", price: 25, image: "", category: "" },
+    { name: "Sushi Masterclass", country: "Japan", price: 30 },
+    { name: "French Cuisine Workshop", country: "France", price: 35 },
+    { name: "Mexican Cooking Fiesta", country: "Mexico", price: 20 },
+    { name: "Thai Street Food Adventure", country: "Thailand", price: 25 },
+    { name: "Mediterranean Flavors Experience", country: "Greece", price: 30 },
+    { name: "Indian Spice Journey", country: "India", price: 25 },
+    { name: "BBQ Grill & Chill", country: "United States", price: 20 },
+    { name: "Vegan Delights Discovery", country: "Various", price: 30 },
+    { name: "Baking Bonanza", country: "Various", price: 25 },
+  ];
+
+router.get('/', (req, res) => {
+    res.json(events)
+})
+
 module.exports = router
--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
 
-// app.use(cors())
+app.use(cors())
 app.use(express.json())
+
+const eventRouter = require('./routes/events')
+app.use('/events', eventRouter)
 
 app.listen(3001, () => {
     console.log('app is listening on port 3001')
